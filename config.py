ad3b01fa14f854fcc2b2df23b78ef4187567106d
mahbubedits7-max <mahbubedits7@gmail.com>
2025-12-15 10:44:41 +0600

    Update config.py
    Signed-off-by: mahbubedits7-max <mahbubedits7@gmail.com>

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+BOT_TOKEN = os.getenv("8182617462:AAEWSUfe2UVNwN6XWPzSEqxdmtp3Js_7XNE")
+ADMIN_ID = int(os.getenv((7779433887))
